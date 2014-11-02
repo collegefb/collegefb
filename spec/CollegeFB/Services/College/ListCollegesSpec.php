46958c35593b6320c93bc6c973f180b6fd017f6a
javiyt <me@javi.yt>
2014-11-02 08:36:16 +0100

    Removed unneeded iterators conversion

--- spec/CollegeFB/Services/College/ListCollegesSpec.php
+++ spec/CollegeFB/Services/College/ListCollegesSpec.php
@@ -5,12 +5,13 @@ namespace spec\CollegeFB\Services\College;
 use PhpSpec\ObjectBehavior;
 use CollegeFB\Repositories\CollegeInterface;
 use CollegeFB\Factories\College as CollegeFactory;
+use CollegeFB\Iterators\College as CollegeIterator;
 
 class ListCollegesSpec extends ObjectBehavior
 {
     public function let(CollegeInterface $repository, CollegeFactory $factory)
     {
-        $repository->listAll(0, 25)->willReturn(array(1, 2));
+        $repository->listAll(0, 25)->willReturn(new CollegeIterator(array(1, 2)));
 
         $this->beConstructedWith($repository, $factory);
     }
@@ -22,6 +23,6 @@ class ListCollegesSpec extends ObjectBehavior
 
     public function it_return_the_list_of_colleges_requested()
     {
-        $this->run(array('page' => 0))->shouldReturnAnInstanceOf('ArrayIterator');
+        $this->run(array('page' => 0))->shouldReturnAnInstanceOf('CollegeFB\Iterators\College');
     }
 }
--- src/Services/College/ListColleges.php
+++ src/Services/College/ListColleges.php
@@ -3,7 +3,6 @@
 namespace CollegeFB\Services\College;
 
 use CollegeFB\Services\CollegeAbstract;
-use ArrayIterator;
 
 class ListColleges extends CollegeAbstract
 {
@@ -12,13 +11,7 @@ class ListColleges extends CollegeAbstract
     public function run(array $options)
     {
         $limit = !empty($options['limit']) ? $options['limit'] : self::LIMIT;
-        $colleges = $this->repository->listAll($options['page'], $limit);
 
-        $found = new ArrayIterator();
-        foreach ($colleges as $college) {
-            $found->append($college);
-        }
-
-        return $found;
+        return $this->repository->listAll($options['page'], $limit);
     }
 }
