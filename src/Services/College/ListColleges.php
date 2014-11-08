562ab1814bbc7dbd6c2e0dba9fc5b6af1766d3ce
javiyt <me@javi.yt>
2014-11-08 19:16:55 +0100

    More options to list colleges

--- src/Services/College/ListColleges.php
+++ src/Services/College/ListColleges.php
@@ -11,7 +11,16 @@ class ListColleges extends CollegeAbstract
     public function run(array $options)
     {
         $limit = !empty($options['limit']) ? $options['limit'] : self::LIMIT;
+        $page = !empty($options['page']) ? $options['page'] : 0;
 
-        return $this->repository->listAll($options['page'], $limit);
+        $conference = !empty($options['conference']) ? $options['conference'] : null;
+
+        if (!is_null($conference)) {
+
+            $this->repository->setQueryParams('conference', $conference);
+
+        }
+
+        return $this->repository->listAll($page, $limit);
     }
 }
